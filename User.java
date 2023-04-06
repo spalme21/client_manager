186ba41a87a1241f4ebd75f517108ed0fc332e18
spalme21 <84144056+spalme21@users.noreply.github.com>
2023-04-06 18:50:36 +0000

    Add fields to User

--- User.java
+++ User.java
@@ -1,3 +1,10 @@
 public class User {
 
+    private String firstName;
+    private String lastName;
+    private String userId;
+    private byte pinHash[];
+    private ArrayList<Book> checkedOutBooks;
+    
+
 }
